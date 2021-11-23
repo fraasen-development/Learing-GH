cdb1fd865dfdd5481ea8c208507ea7ea66504ccf
Finn Reidar Aasen <fraasen@hotmail.com>
2021-11-23 11:19:54 +0100

    Initial Commit

---
+++ LearingGH/LearingGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearingGHApp.swift
+//  LearingGH
+//
+//  Created by Finn Reidar Aasen on 23/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearingGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
